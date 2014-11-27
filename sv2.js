5002f725b27d717372d843b56923ec4d5deadf3a
parseb <306.arsene@gmail.com>
2014-11-27 21:52:25 +0200

    Update sv2.js

--- sv2.js
+++ sv2.js
@@ -1,10 +1,28 @@
+$(document).ready( function() {
+	
 
-$( "#space" ).mousemove(function( event ) {
-var msg = "Handler for .mousemove() called at ";
-msg += event.pageX + ", " + event.pageY;
-$( "#space" ).append( "<div>" + msg + "</div>" );
+		if ($("#space").mousemove()) { $("#space").on("mousemove", function(m) {
+
+			window.i = $("#space").children().size();
+
+			$("#space").on("mousemove", function( event ) {
+			$("#space").append( "<div class='container" + window.i.toString() + "'></div>" );
+			$(".container" + window.i.toString()).css({"top": event.pageY, "left": event.pageX});
+
+
+		$(".container").on("mouseleave", function() {
+			 window.color = Math.floor(Math.random()*16777215).toString(16);
+			 
+
+			 $("#space").append(" <div class='container"+ window.i.toString() +"'> </div>");
+			 $(".container"+ window.i.toString()).css({"top": m.pageY, "left": m.pageX, "background-color": "#"+window.color});
+
+	});	
+
+	});
+
+	});		
+		};
+
+			
 });
-
-
-
-
